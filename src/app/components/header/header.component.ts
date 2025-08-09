fa80227eff29946c01293cc98869e451f98c3d9a
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2025-08-09 12:24:44 +0700

    feat : Update Navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { HomeComponent } from '../home/home.component';
 
@@ -7,6 +7,13 @@ import { HomeComponent } from '../home/home.component';
   standalone: true,
   imports: [NavbarComponent, HomeComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isScrolled: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    this.isScrolled = window.scrollY > 0;
+  }
+}
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  // Properti untuk menu hamburger
+  isMenuOpen: boolean = false;
+
+  // Properti BARU untuk mendeteksi scroll
+  isScrolled: boolean = false;
+
+  constructor() { }
+
+  // Fungsi untuk menu hamburger
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  // Fungsi BARU yang akan berjalan setiap kali user scroll
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    // Cek jika posisi scroll vertikal lebih besar dari 0
+    this.isScrolled = window.scrollY > 0;
+  }
+}
