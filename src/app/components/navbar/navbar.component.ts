4331c7bc7b316d5ec11b98f0e810ca44a3e4d826
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2025-08-09 13:33:31 +0700

    feat : Update Header and Home

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,7 +5,7 @@ import {Component, HostListener} from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css',
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   // Properti untuk menu hamburger
