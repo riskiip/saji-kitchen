b241a4d63444ac40dfd7822a5ebca4984ca8b3e4
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2025-08-23 14:27:34 +0700

    fix : Fix review section

--- src/app/components/customer-feedback/customer-feedback.component.ts
+++ src/app/components/customer-feedback/customer-feedback.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
+
+interface Review {
+  nama: string;
+  review: string;
+  score: number; // Angka dari 1 sampai 5
+  foto: string; // Path ke file gambar atau URL
+}
 
 @Component({
   selector: 'app-customer-feedback',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './customer-feedback.component.html',
-  styleUrl: './customer-feedback.component.css',
+  styleUrl: './customer-feedback.component.scss',
 })
-export class CustomerFeedbackComponent {}
+export class CustomerFeedbackComponent {
+  // Data ulasan statis disimpan di sini
+  reviews: Review[] = [
+    {
+      nama: "Farhan Pratama",
+      review: "Worth the price",
+      score: 5,
+      foto: "assets/images/profile-male-1.jpg" // Ganti dengan path foto Anda
+    },
+    {
+      nama: "Sisca",
+      review: "dimsum mentainya enakk, ukuran dimsumnya juga besar dan berasa daging ayamnyaa 💯",
+      score: 5,
+      foto: "assets/images/profile-female-1.jpg" // Ganti dengan path foto Anda
+    },
+    {
+      nama: "Ayu Galuh",
+      review: "Enak bangetttt, daging nya banyakk. Mentai nya juga mantapp bangett",
+      score: 5,
+      foto: "assets/images/profile-male-2.jpg" // Ganti dengan path foto Anda
+    },
+    {
+      nama: "Bernanda F. Putri",
+      review: "yummy sekalii\n" +
+        "dagingnya terasaa, mentainya juga enakkkk",
+      score: 5,
+      foto: "assets/images/profile-female-2.jpg" // Ganti dengan path foto Anda
+    }
+  ];
+
+  constructor() { }
+
+  getStars(rating: number): number[] {
+    return Array(Math.round(rating)).fill(0);
+  }
+}
